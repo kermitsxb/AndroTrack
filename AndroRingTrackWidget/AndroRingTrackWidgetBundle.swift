0f78eee10b7a43ed4be56f08093b94ec18f72423
Thomas Stocker <thomas.stocker.pro@gmail.com>
2026-08-24 14:31:37 +0200

    feat(widget): scaffold AndroRingTrackWidget extension target

---
+++ AndroRingTrackWidget/AndroRingTrackWidgetBundle.swift
@@ -0,0 +1,38 @@
+import WidgetKit
+import SwiftUI
+
+struct PlaceholderEntry: TimelineEntry {
+    let date: Date
+}
+
+struct PlaceholderProvider: TimelineProvider {
+    func placeholder(in context: Context) -> PlaceholderEntry {
+        PlaceholderEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (PlaceholderEntry) -> Void) {
+        completion(PlaceholderEntry(date: Date()))
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<PlaceholderEntry>) -> Void) {
+        completion(Timeline(entries: [PlaceholderEntry(date: Date())], policy: .never))
+    }
+}
+
+struct PlaceholderWidget: Widget {
+    let kind: String = "PlaceholderWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: PlaceholderProvider()) { _ in
+            Text("AndroRingTrack")
+        }
+        .supportedFamilies([.systemSmall])
+    }
+}
+
+@main
+struct AndroRingTrackWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PlaceholderWidget()
+    }
+}
